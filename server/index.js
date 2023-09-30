143d0a52ce256d6f99cab618fae1e3b14ba4be5a
aviral gupta <aviralg2002@gmail.com>
2023-09-30 11:46:43 +0530

    testing

--- server/index.js
+++ server/index.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/test", (req, res) => {
+    res.json({message: "Its working"});
+});
+
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
